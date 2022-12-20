ac1f674f69f4e4a4ac20fa9364e5c66c74673588
comradexlight <comradexlight@gmail.com>
2022-12-20 17:04:23 +0300

    added global storage - CtxStorage from vkbottle

--- bot_vkbottle.py
+++ bot_vkbottle.py
@@ -1,7 +1,6 @@
 import requests
-from dataclasses import dataclass
 from os import remove
-from vkbottle import API, Keyboard, EMPTY_KEYBOARD, Text, KeyboardButtonColor, PhotoMessageUploader
+from vkbottle import API, Keyboard, EMPTY_KEYBOARD, Text, KeyboardButtonColor, PhotoMessageUploader, CtxStorage
 from vkbottle.bot import Message, Bot
 from loguru import logger
 from typing import List
@@ -15,8 +14,10 @@ from maintenance import string_with_born_to_age
 api = API(GROUP_TOKEN)
 user_api = API(USER_TOKEN)
 bot = Bot(api=api)
-bot.candidate_number = 0
-bot.user = "" # это времяночка
+ctx_storage = CtxStorage()
+ctx_storage.set("candidate_number", 0)
+ctx_storage.set("user_id", None)
+ctx_storage.set("candidates", None)
 
 async def _candidates_search(age: int, sex_id: int, city_id: str) -> List:
     sex_id = 1 if sex_id == 2 else 2
@@ -26,16 +27,15 @@ async def _candidates_search(age: int, sex_id: int, city_id: str) -> List:
     return candidates
 
 
-async def _show_candidate(candidates: List, candidate_number: int, user_id: int):
+async def _show_candidate(candidates: List, user_id: int, candidate_number: int, is_favourite=False ):
     candidate = Candidate(vk_id=candidates.items[candidate_number].id,
         first_name=candidates.items[candidate_number].first_name,
         last_name=candidates.items[candidate_number].last_name,
         vk_link=f"https://vk.com/id{candidates.items[candidate_number].id}",
-        is_favourite=False,
-        user_id=user_id)
-    
-    db_interaction.add_person_to_db(candidate)                                           
+        is_favourite=is_favourite,
+        user_id=user_id) # TODO: тут времяночка
     return candidate
+
 
 async def _show_photo(candidate_id: int) -> str:
     photos = await user_api.photos.get(owner_id=candidate_id,
@@ -70,7 +70,6 @@ async def start_hadler(message: Message):
         .add(Text("Нет", {"command": "/exit"}),
          color=KeyboardButtonColor.NEGATIVE)
     ).get_json()
-
     await message.answer("Ищешь пару?", keyboard=keyboard)
 
 
@@ -87,17 +86,17 @@ async def get_user_info_handler(message: Message):
     user_data = await bot.api.users.get(message.from_id,
      fields=["city", "sex", "bdate"])
 
-    bot.user = User(vk_id=user_data[0].id,
+    user = User(vk_id=user_data[0].id,
                 first_name=user_data[0].first_name,
                 last_name=user_data[0].last_name,
                 age=string_with_born_to_age(user_data[0].bdate),
                 sex_id=user_data[0].sex.value,
                 city_id=user_data[0].city.id)
+    db_interaction.add_person_to_db(user)
+    ctx_storage.set("user", user)
 
-    db_interaction.add_person_to_db(bot.user)
-    candidates = await _candidates_search(age=bot.user.age,
-                                         sex_id=bot.user.sex_id,
-                                         city_id=bot.user.city_id)
+    # TODO: может быть , тут найти и локально сохранить candidates
+
     await message.answer("Начинаю поиск...",
                         keyboard=keyboard)
 
@@ -113,21 +112,26 @@ async def show_candidate_handler(message: Message):
     ).get_json()
 
     #TODO: (?) тут вызвать функцию получния из бд данных юзера, которая возвращает объект user класса User
-
     #это времяночка:
-    candidates = await _candidates_search(age=bot.user.age,
-                                         sex_id=bot.user.sex_id,
-                                         city_id=bot.user.city_id)
-
-    if bot.candidate_number >= len(candidates.items):
+    user = ctx_storage.get("user")
+    candidate_number = ctx_storage.get("candidate_number")
+    if ctx_storage.get("candidates") is None:
+        candidates = await _candidates_search(age=user.age,
+                                             sex_id=user.sex_id,
+                                             city_id=user.city_id)
+        ctx_storage.set("candidates", candidates)
+    else:
+        candidates = ctx_storage.get("candidates")
+    if candidate_number >= len(candidates.items):
         await message.answer("Просмотрены все кандидаты на данный момент")
 
-    candidate = await _show_candidate(candidates, bot.candidate_number)
+    candidate = await _show_candidate(candidates, 1, candidate_number) #тут времяночка, нужен реальный user_id
+    db_interaction.add_person_to_db(candidate)         
+    candidate_number += 1
 
-
-    bot.candidate_number += 1
+    ctx_storage.set("candidate_number", candidate_number)
+   
     # photos = await _show_photo(candidate.vk_id)
-
     #TODO: тут вызвать функцию получения топ-3 фото кандидата
    
     #это "времяночка". тут должно быть top3_photots = await _get_top3_photos(candidate.id)
@@ -137,12 +141,12 @@ async def show_candidate_handler(message: Message):
 
     attachment = await _get_attachment(top3_photos)
     
-    await message.answer(f"По твоим параметрам нашлось {len(candidates.items)} человек.\n Вот {bot.candidate_number}-ый:\n{candidate}",
+    await message.answer(f"По твоим параметрам нашлось {len(candidates.items)} человек.\n Вот {candidate_number}-ый:\n{candidate.first_name} {candidate.last_name}\n Ссылка на профиль: {candidate.vk_link}",
         attachment=attachment,
         keyboard=keyboard)
 
 
-@bot.on.message(payload={"command": "/exit", "candidate_number": any})
+@bot.on.message(payload={"command": "/exit"})
 async def exit_handler(message: Message):
     await message.answer("Жаль, что ты уходишь. Приходи ещё. "\
         "Чтобы начать снова напиши /start",
