1b9d5c3be56e1f710b4f86102651f97f2013e00e
Alexey Kibyakov <lexa111lexa6@mail.ru>
2022-12-25 13:13:33 +0300

    edit pep8

--- db_interaction.py
+++ db_interaction.py
@@ -49,11 +49,11 @@ def add_photos_to_db(photo: Photo):
 
 
 def show_favourite_list() -> list:
-    '''Receiving list of favourite candidates
+    '''Receiving list of favourite candidates 
     in format [candidate, [candidate_photos]]'''
     fav_list = []
     for c_ in session.query(Candidate).join(Photo.candidate).\
-        filter(Candidate.is_favourite is True):
+        filter(Candidate.is_favourite == True):
         photo_list = []
         for p_ in session.query(Photo).join(Candidate.photos).\
             filter(Photo.candidate_vk_id == c_.vk_id):
